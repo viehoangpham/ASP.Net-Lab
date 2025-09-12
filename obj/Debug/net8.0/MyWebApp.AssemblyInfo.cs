810f4e77545f6490947b626cd36007b89902ceab
viehoangpham <fhoang25@gmail.com>
2025-09-12 16:04:04 +0700

    add link student list

--- obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+470fe98d666a08525937a1ff37987ab5a15e8f52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a45d19f47298f03f3270954290c418db56c390b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
