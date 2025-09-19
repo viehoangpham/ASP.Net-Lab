fc0ea8c894964e1963d3ebc2205e1f52e45d51b3
hoangbru <hoangbru07@gmail.com>
2025-09-19 18:40:36 +0700

    Lab-2

---
+++ ViewComponents/RenderViewComponent.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using MyWebApp.Models;
+
+public class RenderViewComponent: ViewComponent
+{
+  private List<MenuItem> MenuItems = new List<MenuItem>();
+  public RenderViewComponent()
+  {
+    MenuItems = new List<MenuItem>(){
+      new MenuItem() { Id=1, Name="Branches", Link="/Admin/Branch/List" },
+      new MenuItem() { Id=2, Name="Students", Link="/Admin/Student/List" },
+    };
+  }
+  public async Task<IViewComponentResult> InvokeAsync()
+  {
+    return View("RenderLeftMenu", MenuItems);
+  }
+}
--- obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a45d19f47298f03f3270954290c418db56c390b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82477d39c1fdd14bd15b87d7dd27f97a4b0eb576")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
